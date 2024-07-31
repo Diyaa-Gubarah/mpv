6ecfa7c8221cde6261b038bb7fac8ddc8d13a46a
Diyaa <36938682+Diyaa-Gubarah@users.noreply.github.com>
2024-07-31 20:07:44 +0300

    add basename to router

--- src/main.tsx
+++ src/main.tsx
@@ -24,7 +24,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/mpv">
       <Provider store={store}>
         <Layout>
           <Routes>
